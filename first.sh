b0b2ecaa3706bbdcf79f62dce568cec87ebfc018
hazemelbaz <hazem_baz@hotmail.com>
2020-08-08 21:37:18 +0300

    Here we lean how to add variable and use it in example

--- first.sh
+++ first.sh
@@ -4,3 +4,8 @@
 # This is comment line, it will not appear when you are run the file
 
 echo "Hello Hazem Elbaz in Bach Scripting Tutorial"
+
+# Here we create variables and use these variables
+
+name=tea                # here we declear variable
+echo "The word $name contains ${#name} chars."      # here we $name for use value of variable, and ${#name} will get count of characeters
